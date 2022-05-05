42573c4be95597d11872d029f2bd68caade9b92d
Daniel Costa <daniel.costa@nhs.scot>
2022-05-05 11:40:40 +0100

    Updates

---
+++ precache-manifest.4bff9d0cacf3bc57447af0cc770d2946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8809f1389876962fb3ac",
+    "url": "/wheel-of-misfortune/static/css/main.f8fb2649.chunk.css"
+  },
+  {
+    "revision": "8809f1389876962fb3ac",
+    "url": "/wheel-of-misfortune/static/js/main.0aa35b4d.chunk.js"
+  },
+  {
+    "revision": "6e36db04c5ccbc063dc8",
+    "url": "/wheel-of-misfortune/static/js/runtime~main.49bbe151.js"
+  },
+  {
+    "revision": "0039ae88f18260901f66",
+    "url": "/wheel-of-misfortune/static/js/2.4eb3da61.chunk.js"
+  },
+  {
+    "revision": "53eb36ad4c31d85021ca",
+    "url": "/wheel-of-misfortune/static/js/3.2ca3b74e.chunk.js"
+  },
+  {
+    "revision": "3500eb7a75bb004f7665b72196ec8b56",
+    "url": "/wheel-of-misfortune/index.html"
+  }
+];
