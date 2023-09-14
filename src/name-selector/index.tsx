ea40d4956fbc433d05ac00206c993385a54faae1
Daniel Costa <daniel.costa@nhs.scot>
2023-09-14 11:50:45 +0100

    Add prize wheel

--- src/name-selector/index.tsx
+++ src/name-selector/index.tsx
@@ -12,90 +12,92 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Grid from '@mui/material/Grid';
 
-interface ChipData {
-    key: string;
-    label: string;
+import { Wheel } from 'react-custom-roulette';
+
+interface WheelData {
+    option: string;
 }
-
-const others = [
-    { key: 'Anna', label: 'Anna' },
-    { key: 'Scott', label: 'Scott' },
-    { key: 'Ross', label: 'Ross' },
-    { key: 'Kalum', label: 'Kalum' },
-    { key: 'JP', label: 'JP' },
-    { key: 'Kevin', label: 'Kevin' },
-    { key: 'Mani', label: 'Mani' }
-]
-
-const engineers = [
-    { key: 'Alan', label: 'Alan' },
-    { key: 'Analben', label: 'Analben'},
-    { key: 'Callum', label: 'Callum' },
-    { key: 'Danny', label: 'Danny' },
-    { key: 'Paddy', label: 'Paddy' },
-    { key: 'Rafal', label: 'Rafal' }
-]
 
 const ListItem = styled('li')(({ theme }) => ({
     margin: theme.spacing(0.5),
 }));
 
 export default function ChipsArray() {
-    const [chips, setChips] = React.useState<any[]>([]);
+    const [data, setData] = React.useState<WheelData[]>([]);
+
     const [nameToAdd, setNameToAdd] = React.useState(String);
-    const [deleteOnPickToggle, setDeleteOnPickToggle] = React.useState(false);
-    const [namePicked, setNamePicked] = React.useState<ChipData | undefined>();
+    const [deleteOnPickToggle, setDeleteOnPickToggle] = React.useState(true);
+
+    const [mustSpin, setMustSpin] = React.useState(false);
+    const [prizeNumber, setPrizeNumber] = React.useState(-1);
 
     React.useEffect(() => {
         switch(window.location.search) {
-            case '?ncds':
-                setChips([...engineers, ...others]);
-                break;
-            case '?ncdse':
-                setChips([...engineers]);
+            case '?dss':
+                setData( [
+                    { option: 'Danny' },
+                    { option: 'Paddy' },
+                    { option: 'Kal' },
+                    { option: 'Ross' },
+                    { option: 'Callum' },
+                    { option: 'JP' },
+                ]);
                 break;
             default:
                 if (window.location.search.startsWith("?")) {
                     const searchNames = (window.location.search).substring(1).split(",");
                     const names = new Array();
                     searchNames.forEach( (name) => {
-                        names.push({"key": name, "label": name});
+                        names.push({"option": name});
                     })
-                    setChips([...names]);
+                    setData([...names]);
                 }
                 break;
-        }  
+        }
       }, []);
 
-    const handleDelete = (chipToDelete: ChipData) => () => {
-        if (chipToDelete) {
-            setChips((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+    const handleDelete = (element: WheelData) => () => {
+        if (element) {
+            setData((data) => data.filter((e) => e.option !== element.option));
         }
     };
 
     const onKeyPress = (key: React.Key) => {
         if (key === 'Enter') {
-            setChips((chips) => {
-                return [...chips, { key: nameToAdd, label: nameToAdd }];
+            setData((data) => {
+                return [...data, { option: nameToAdd }];
             });
             setNameToAdd("");
         }
-
     }
 
-    const pickAName = () => {
-        let pickedName = chips[Math.floor(Math.random() * chips.length)];
-        setNamePicked(pickedName);
-        if (deleteOnPickToggle) {
-            setChips((chips) => chips.filter((chip) => chip.key !== pickedName.key));
+    const handleSpinClick = () => {
+
+        let tempData = data;
+        if (prizeNumber != -1 && deleteOnPickToggle) {
+            tempData = data.filter((e) => e !== data[prizeNumber]);
+            setData(tempData);
         }
-    };
+
+        if (!mustSpin) {
+            const newPrizeNumber = Math.floor(Math.random() * tempData.length);
+            setPrizeNumber(newPrizeNumber);
+            setMustSpin(true);
+        }
+    }
 
     return (
         <div>
             <hr className="marginedHR" />
 
-            <TextField id="namesInput" variant="filled" label="Enter name" color="secondary" value={nameToAdd} onChange={e => setNameToAdd(e.target.value)} onKeyPress={e => onKeyPress(e.key)} />
+            <TextField
+                id="namesInput"
+                variant="filled"
+                label="Enter name"
+                color="secondary"
+                value={nameToAdd}
+                onChange={e => setNameToAdd(e.target.value)} onKeyPress={e => onKeyPress(e.key)}
+            />
 
             <Paper id="chipsPaper"
                 sx={{
@@ -109,15 +111,15 @@ export default function ChipsArray() {
                 component="ul"
             >
 
-                {chips.map((data) => {
+                {data.map((e) => {
 
                     return (
-                        <ListItem key={data.key}>
+                        <ListItem key={e.option}>
                             <Chip
-                                label={data.label}
+                                label={e.option}
                                 color='secondary'
-                                avatar={<Avatar>{data.label[0]}</Avatar>}
-                                onDelete={handleDelete(data)}
+                                avatar={<Avatar>{e.option[0]}</Avatar>}
+                                onDelete={handleDelete(e)}
                             />
                         </ListItem>
                     );
@@ -129,17 +131,46 @@ export default function ChipsArray() {
             <Grid container>
             <Grid item xs={6}>
                     <FormGroup>
-                        <FormControlLabel control={<Switch />} label="Delete name on getting picked" labelPlacement="start" checked={deleteOnPickToggle} onChange={e => setDeleteOnPickToggle(!deleteOnPickToggle)} />
+                        <FormControlLabel
+                            control={<Switch />}
+                            label="Delete name on getting picked"
+                            labelPlacement="start"
+                            checked={deleteOnPickToggle}
+                            onChange={e => setDeleteOnPickToggle(!deleteOnPickToggle)}
+                        />
                     </FormGroup>
                 </Grid>
                 <Grid item xs={4}>
-                    <Button variant="contained" size="large" onClick={pickAName}> Pick a Name </Button>
+                    <Button variant="contained" size="large" onClick={handleSpinClick}> Pick a Name </Button>
                 </Grid>
-                
             </Grid>
-            <div className='centreGridCell'>
-                <h1 id='pickedName'>{namePicked ? namePicked.label : ""}</h1>
-            </div>
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                sx={{marginTop: '36px'}}
+                >
+                <Grid item >
+                    {data.length > 1 &&
+                    <>
+                        <Wheel
+                            backgroundColors={['#FD8A8A', '#FFCBCB', '#9EA1D4', '#F1F7B5', '#A8D1D1', '#DFEBEB']}
+                            mustStartSpinning={mustSpin}
+                            prizeNumber={prizeNumber}
+                            data={data}
+                            spinDuration={0.4}
+                            onStopSpinning={() => {
+                                setMustSpin(false);
+
+                            }}
+                        />
+                    </>
+                    }
+                    {data.length == 1 && <h1 id='pickedName'>{data[0].option}</h1>}
+                </Grid>
+            </Grid>
 
         </div >
     );
